7d951fd09c247764ea8c0555979cff4dcb411b2d
liyingxin97 <62927219+liyingxin97@users.noreply.github.com>
2020-03-31 02:34:58 -0400

    Add files via upload

---
+++ physical therapy/scripts/base.js
@@ -0,0 +1,24 @@
+//This is the base javascript filefunction billme()
+
+function billme()
+{
+        var v1= document.order.porkQty.value;
+    
+        //These lines define the variables.
+        
+            v= parseInt(v1);
+            v2= parseInt(v2);
+            v3= parseInt(v3);
+            v4= parseInt(v4);
+            v5= parseInt(v5);
+            v6= parseInt(v6);
+      
+      
+            order.subtotalBox.value= ((5.8*24) + v1*52)/(13*0.2588190451)
+       }
+        
+        function porkImage() {
+            document.getElementById("mainpic").src="images/Therapy.png"
+        }
+        
+     
